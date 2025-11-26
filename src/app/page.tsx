499246cac623e66d4027515148a1e646542678d3
Andrey Lebedev <alebedev424@gmail.com>
2025-11-26 14:57:42 +0300

    fix: обернул RestoreLastPage в Suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 "use client";
 
-import RestoreLastPageComponent from "@/components/RestoreLastPage/RestoreLastPage";
+import RestoreLastPage from "@/components/RestoreLastPage/RestoreLastPage";
 import SearchForm from "@/components/SearchForm/SearchForm";
 import styles from "@/styles/Home.module.scss";
+import { Suspense } from "react";
 
 export default function HomePage() {
   return (
     <main className={styles.container}>
-      <RestoreLastPageComponent />
+      <Suspense fallback={<div>Redirecting...</div>}>
+        <RestoreLastPage />
+      </Suspense>
       <h1 className={styles.title}>Поиск отелей</h1>
       <SearchForm />
     </main>
