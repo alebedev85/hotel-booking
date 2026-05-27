07772bedc4168192ade7fd47a811ddc6e125345a
Andrey Lebedev <alebedev424@gmail.com>
2026-05-27 13:21:58 +0300

    feature: добавил глобальный прилоудер, поправил стили в layout.tsx, убрал postinstall: prisma generate

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,16 @@
 import AuthChecker from "@/components/AuthChecker/AuthChecker";
 import Header from "@/components/Header/Header";
 import RestoreLastSearchComponent from "@/components/RestoreLastSearchComponent/useRestoreLastSearch";
+import Footer from "@/components/Footer/Footer";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
 import "@/styles/globals.scss";
-import "leaflet/dist/leaflet.css";
-import { Inter, Playfair_Display } from 'next/font/google';
-import 'material-symbols';
+import "material-symbols";
 import type { Metadata } from "next";
+import { Inter, Playfair_Display } from "next/font/google";
 import { Suspense } from "react";
 import { Providers } from "./providers";
-import Footer from "@/components/Footer/Footer";
+import NextTopLoader from "nextjs-toploader";
 
 export const metadata: Metadata = {
   title: "Бугорок!",
@@ -17,16 +19,16 @@ export const metadata: Metadata = {
 
 // Настраиваем Inter
 const inter = Inter({
-  subsets: ['latin', 'cyrillic'],
-  weight: ['300', '400', '600', '800'],
-  variable: '--font-inter', // создаем CSS-переменную
+  subsets: ["latin", "cyrillic"],
+  weight: ["300", "400", "600", "800"],
+  variable: "--font-inter", // создаем CSS-переменную
 });
 
 // Настраиваем Playfair Display
 const playfair = Playfair_Display({
-  subsets: ['latin', 'cyrillic'],
-  weight: ['400', '700'],
-  variable: '--font-playfair',
+  subsets: ["latin", "cyrillic"],
+  weight: ["400", "700"],
+  variable: "--font-playfair",
 });
 
 export default function RootLayout({
@@ -36,8 +38,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ru" className={`${inter.variable} ${playfair.variable}`}>
-      <head/>
+      <head />
       <body suppressHydrationWarning={true}>
+      <NextTopLoader 
+          color="var(--color-primary)"
+          initialPosition={0.08}
+          crawlSpeed={200}
+          height={3}
+          crawl={true}
+          showSpinner={false}
+          easing="ease"
+          speed={200}
+        />
         <Providers>
           <AuthChecker />
           <Suspense fallback={<div>Redirecting...</div>}>
